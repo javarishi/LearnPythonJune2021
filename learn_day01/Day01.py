04a32d7082529f18ceb94de4f523e3c889f26e6b
Rishi Arun <rishi.h2kinfosys@gmail.com>
2021-06-18 05:55:50 +0530

    Day01
    Day01

---
+++ learn_day01/Day01.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("Another Line after New Line")
+# Single Line Comment
+
+'''
+Multi 
+Line 
+Comment
+'''
+
+"""
+Used for Python Documentation
+three double quotes
+"""
---
+++ learn_day01/Variables.py
@@ -0,0 +1,14 @@
+age = 21
+Age = 22
+AGE = 23
+name = "David"
+discount = 2.99
+complex_number = 1 + 4j
+# variableName = assignment
+
+loyalty_discount_for_county_customer = 10.00
+age21 = 21
+age_18 = 18
+_age = 33
+
+print(age, Age, AGE, _age)
